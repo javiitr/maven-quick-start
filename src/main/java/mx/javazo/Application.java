8b0c5caede008cb0d5180ce5b7be0ab1f61d1e34
Javier TR <javiitr@gmail.com>
2022-04-13 22:01:05 -0600

    Use StringUtils and addesd dependency to lang3

--- src/main/java/mx/javazo/Application.java
+++ src/main/java/mx/javazo/Application.java
@@ -3,7 +3,18 @@ package mx.javazo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
+  public Application() {
+    System.out.println("Inside Application");
+  }
+
+  public int countWords(String words) {
+    String[] separateWords = StringUtils.split(words, ' ');
+    return (separateWords == null) ? 0 : separateWords.length;
+  }
+
   public void greet() {
     List<String> greetings = new ArrayList<>();
     greetings.add("Hello");
@@ -12,13 +23,15 @@ public class Application {
       System.out.println("Greeting: "+greeting);
     }
   }
-  public Application() {
-    System.out.println("Inside Application");
-  }
 
   public static void main(String[] args) {
     System.out.println ("Starting Application");
     Application app = new Application();
     app.greet();
+    
+    String words = "Javazo test method";
+    int count = app.countWords(words);
+    System.out.println("Words: "+words);
+    System.out.println("Word Count: "+count);
   }
 }
